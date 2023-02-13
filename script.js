6c8a0417feb829ffcc9f0aae63eb052df01e7aba
crab-milk <72271395+crab-milk@users.noreply.github.com>
2023-02-12 18:09:46 -0600

    Realign buttons

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // alert('hello world');
+const menu_buttons = document.querySelectorAll(".menu_buttons");
+const checkout_subtotal = document.querySelectorAll(".subtotal");
+
+let subtotal = 0;
+
+menu_buttons.forEach(function (set)) {
+    let item_total = 0;
+
+    button_add.addEventListener("click", function () {
+        item_total++;
+
+    })
+
+    button_remove.addEventListener("click", function () {
+        if (item_total > 0) {
+            item_total--;
+        }
+    })
+}
