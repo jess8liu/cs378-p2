5d378fbe6c50aef45c31b868191d51c887eadaa2
crab-milk <72271395+crab-milk@users.noreply.github.com>
2023-02-13 14:06:07 -0600

    Update items

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Get all of the menu items and checkout
 const menu_label = document.querySelectorAll(".menu-label");
 const checkout = document.querySelectorAll(".checkout");
+const button_order = set.querySelector(".order");
+const button_clear = set.querySelector(".clear");
 
 let subtotal = 0;
 document.getelementbyid ("subtotal").innerHTML = 0;
@@ -11,38 +13,37 @@ menu_label.forEach(function (set) {
     const button_remove = set.querySelector(".remove");
 
     // Get item price and total and set it to 0
-    const item_price = parseInt(set.querySelector(".menu-price").dataset.price-val);
-    let item_total = 0;
+    const item_price = parseInt(set.querySelector(".menu-price").dataset.price);
+    let num_items = 0;
+    var item_total = document.getElementById("menu-item-total");
+    var checkout_subtotal = document.getElementById("subtotal");
 
     button_add.addEventListener("click", function () {
         // Add to items total and increase subtotal price
-        item_total++;
+        num_items++;
         subtotal += item_price;
+        item_total.innerHTML = toString(num_items);
+        checkout_subtotal.innerHTML = toString(subtotal);
     });
 
     button_remove.addEventListener("click", function () {
         // Ensure that the item_total does not go into negatives
         if (item_total > 0) {
-            item_total--;
+            num_items--;
             subtotal -= item_price;
+            item_total.innerHTML = toString(num_items);
+            checkout_subtotal.innerHTML = toString(subtotal);
         }
-    });
-});
-
-checkout.forEach(function (set) {
-    // Get order and clear buttons
-    const button_order = set.querySelector(".order");
-    const button_clear = set.querySelector(".clear");
-    
-    button_order.addEventListener("click", function () {
-        // Do an alert
-
     });
 
     button_clear.addEventListener("click", function () {
         // Clear all orders and subtotal
-        document.getelementbyid ("subtotal").innerHTML = 0;
+        document.getElementById("subtotal").innerHTML = "0";
+    });
+ 
+    button_order.addEventListener("click", function () {
+        // Do an alert
+
     });
 });
-
 // alert('hello world');
